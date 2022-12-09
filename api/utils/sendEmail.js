ea2d564e25cac5a1fb74b42e8ae6cd470980816e
ruchita31 <13ruchitaiyer@gmail.com>
2022-12-09 16:37:42 -0500

    utils

---
+++ api/utils/sendEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer')
+
+const sendEmail = async options => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure:true,
+    auth: {
+      user: process.env.SMTP_EMAIL,
+      pass: process.env.SMTP_PASSWORD
+    }
+  });
+
+  const message = {
+    from: `${process.env.SMTP_NAME_FROM} <${process.env.SMTP_EMAIL_FROM}>`,
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  }
+
+  await transporter.sendMail(message);
+}
+
+module.exports = sendEmail;
